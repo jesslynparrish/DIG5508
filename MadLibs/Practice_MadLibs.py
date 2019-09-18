3a4f7580cb10e9bf6cbcf33c750a1d208071ec14
jesslynparrish <jesslynparrish@gmail.com>
2019-09-18 19:35:11 -0400

    Update Practice_MadLibs.py

--- MadLibs/Practice_MadLibs.py
+++ MadLibs/Practice_MadLibs.py
@@ -5,4 +5,5 @@ verbs = ["runs", "jumps", "eats", "cooks", "toasts", "pets", "flips", "types"]
 
 
 print("The Girl " + random.choice(verbs) + " until she fell asleep")
-print("In the morning she ") + random.choice(verbs) + " half asleep."
+print("In the morning, she ") + random.choice(verbs) + " half asleep."
+print("When lunchtime FINALLY arrived, her dog  " + random.choice(verbs) + " before she leaves for work." )
