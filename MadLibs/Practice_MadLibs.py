6caee30d0d37167298aa6be842049b7617e75f14
jesslynparrish <jesslynparrish@gmail.com>
2019-09-18 19:09:37 -0400

    Create Practice_MadLibs.py

---
+++ MadLibs/Practice_MadLibs.py
@@ -0,0 +1,7 @@
+A tired person who just wants a nap...but must first live through the day
+import random
+
+verbs = ["runs", "jumps", "eats", "cooks", "toasts", "pets", "flips", "types"]
+
+
+print("The Girl " + random.choice(verbs) + " until she fell asleep")
