8962f9e4dde71e83b7b52db3dec5d8240bc25d16
BPerez2021 <BrieanaPerez@gmail.com>
2019-09-18 19:52:33 -0400

    Update Practice_MadLibs.py

--- MadLibs/Practice_MadLibs.py
+++ MadLibs/Practice_MadLibs.py
@@ -1,8 +1,8 @@
 A tired person who just wants a nap...but must first live through the day
 import random
 
-verbs = ["runs", "jumps", "eats", "cooks", "toasts", "pets", "flips", "types"]
-nouns = ["coffee", "car", "egg", "computer", "door", "bed", "pancakes"]
+verbs = ["runs", "jumps", "eats", "cooks", "toasts", "pets", "flips", "types", "chases", "dances", "changes", "corrects", "hides"]
+nouns = ["coffee", "car", "egg", "computer", "door", "bed", "pancakes", "chair", "flower", "chocolate", "pens", "lamp", "shark"]
 
 print("The Girl " + random.choice(verbs) + " until she fell asleep")
 print("In the morning, she " + random.choice(verbs) + " her coffee half asleep.")
@@ -10,3 +10,4 @@ print("When lunchtime FINALLY arrived, her dog  " + random.choice(verbs) + " bef
 print("When she got to work via " + random.choice(nouns) + " her boss was " + random.choice(verbs))
 print("However her co-worker starts " + random.choice(verbs) + " to distract their boss.")
 print("She " + random.choice(verbs) + " into her office, where she encountered " + random.choice(nouns))
+print("Cautiously she walks towards her desk as the " + random.choice(nouns) + " starts to " + random.choice(verbs))
