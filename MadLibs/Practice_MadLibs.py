edcfa4e142b60cc6b3a5dd484134413204aafa1a
jesslynparrish <jesslynparrish@gmail.com>
2019-09-18 19:46:34 -0400

    Update Practice_MadLibs.py

--- MadLibs/Practice_MadLibs.py
+++ MadLibs/Practice_MadLibs.py
@@ -2,8 +2,10 @@ A tired person who just wants a nap...but must first live through the day
 import random
 
 verbs = ["runs", "jumps", "eats", "cooks", "toasts", "pets", "flips", "types"]
-
+nouns = ["coffee", "car", "egg", "computer", "door", "bed", "pancakes"]
 
 print("The Girl " + random.choice(verbs) + " until she fell asleep")
-print("In the morning, she " + random.choice(verbs) + " half asleep.")
+print("In the morning, she " + random.choice(verbs) + " her coffee half asleep.")
 print("When lunchtime FINALLY arrived, her dog  " + random.choice(verbs) + " before she leaves for work." )
+print("When she got to work via " + random.choice(nouns) + " her boss was " + random.choice(verbs))
+print("")
