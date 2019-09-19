1b345aecd70d5d2512da08622bf25d413bb221ed
BPerez2021 <BrieanaPerez@gmail.com>
2019-09-18 20:04:50 -0400

    Update Practice_MadLibs.py

--- MadLibs/Practice_MadLibs.py
+++ MadLibs/Practice_MadLibs.py
@@ -6,6 +6,7 @@ nouns = ["coffee", "car", "egg", "computer", "door", "bed", "pancakes", "chair",
 
 print("The Girl " + random.choice(verbs) + " until she fell asleep")
 print("In the morning, she " + random.choice(verbs) + " her coffee half asleep.")
+print("As she gets ready for work, her " + random.choice(nouns) + " starts to " + random.choice(verbs) + " distracting her from " + random.choice(nouns))
 print("When lunchtime FINALLY arrived, her dog  " + random.choice(verbs) + " before she leaves for work." )
 print("When she got to work via " + random.choice(nouns) + " her boss was " + random.choice(verbs))
 print("However her co-worker starts " + random.choice(verbs) + " to distract their boss.")
