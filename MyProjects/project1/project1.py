8f1bc2dca37cc914ff089958ac78c7ec2701e915
jesslynparrish <jesslynparrish@gmail.com>
2019-10-15 23:20:40 -0400

    Update to project1

--- MyProjects/project1/project1.py
+++ MyProjects/project1/project1.py
@@ -1,14 +1,30 @@
 #%%
-#Exercise 7-1
-print("Hello World")
+#Traveling abroad can be an exciting experience, though preparing can become stressful.
+#One of the more prominent causes of stress is ensuring that you have the correct money for the trip, and one decision that each traveler must make is where is the best place to convert currency for their trip? For example, should you convert your currency before leaving at a currency exchange spot? Should they visit a currency exchange kiosk upon arriving at the airport of their final destination? Or is it better to visit a local bank after checking into your hotel (which is what a lot of my fellow travelers did on a trip to Morocco)? There is also the tried and true ATM. 
 
-    
+#In this currency converter, the user has a few options:
+#-Convert dollars to euros 
+#-Convert dollars to dirhams
+#-Convert euros to dollars 
+
+#To test this converter, complete these conversions:
+
+#Convert 30 dollars to euros using the function to_euro
+
+#Convert 100 euros to dollars using the function to_dollar
+
+#Convert 50 dollars to dirham using the function to_dirham
 #%%
-#Exercise 7-1
-print("I am afraid of this lab")
+def to_euro(d):
+    return d * (0.91)
+
 
 #%%
-#Exercise 7-1
-print("I am so excited for when programming makes a lot of sense and is less intimidating")
+def to_dollar(e):
+    return e * (1.1)
+
+#%%
+def to_dirham(c):
+    return c / (0.27)
 
 #%%
---
+++ MyProjects/project1/readme.md
@@ -0,0 +1,3 @@
+In this free project, I decided to work on exercise 7-2 from the textbook. 
+
+I created a simple currency converter for the example, and chose to convert dollars to euros or dirham, and convert euros to dollars. Part of my decision was in thinking about some of the trips I took when living abroad during my master's (when I lived in Ireland, but was lucky enough to visit Morocco).
