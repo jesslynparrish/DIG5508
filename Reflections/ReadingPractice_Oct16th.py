42fb577681c674cdf6bfcf2681f1d10081f099db
jesslynparrish <jesslynparrish@gmail.com>
2019-10-16 18:00:36 -0400

    updates

---
+++ Reflections/ReadingPractice_Oct16th.py
@@ -0,0 +1,26 @@
+#%%
+len('hello world')
+len('2112')
+len('')
+
+
+#%%
+hi = 'hello world'
+len(hi)
+
+#%%
+len(2112)
+
+#%%
+'hello'[0]
+
+#%%
+'hello world'[0:3]
+
+#%%
+'hello world'[:3]
+
+#%%
+'abcdefgh'[0:9:2]
+
+#%%
