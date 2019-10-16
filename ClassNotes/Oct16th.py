f43c3d066f8ce2293a8c65f5038b5bbdcb43cbf0
jesslynparrish <jesslynparrish@gmail.com>
2019-10-16 18:56:59 -0400

    notes

---
+++ ClassNotes/Oct16th.py
@@ -0,0 +1,35 @@
+#%%
+class MyClass:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+#%%
+import json
+data = {
+    "president": {
+        "name": "Zaphod Beeblebrox",
+        "species": "Betelgeusian"
+    }
+}
+
+
+#%%
+#Week 7 Notes & in-class Work
+Agenda for Tonight's Class:
+Flight of the Code Monkeys
+Generated Novels and NaNoGenMo
+Worked Exercises & Demonstrations
+break
+Gutenberg Exercise
+
+Lab due is delayed another week - due Oct.30th
+ -builds on gutenberg library & tasks to create charts & graphs
+
+IF = Interactive Fiction
+
+
+
+#%%
