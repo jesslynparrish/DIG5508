9e0d5f73ab3d0059da2e23cff5499269f44010b6
jesslynparrish <jesslynparrish@gmail.com>
2019-10-15 21:38:31 -0400

    Updates

---
+++ MyProjects/project1/project1.py
@@ -0,0 +1,14 @@
+#%%
+#Exercise 7-1
+print("Hello World")
+
+    
+#%%
+#Exercise 7-1
+print("I am afraid of this lab")
+
+#%%
+#Exercise 7-1
+print("I am so excited for when programming makes a lot of sense and is less intimidating")
+
+#%%
