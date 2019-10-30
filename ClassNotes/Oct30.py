9970c9600edffc9740010f50901583616270e0c1
jesslynparrish <jesslynparrish@gmail.com>
2019-10-30 18:43:44 -0400

    new notes and notes in Lab2

---
+++ ClassNotes/Oct30.py
@@ -0,0 +1,25 @@
+#Processing and Canvas
+#What did we learn?
+    #File systems & operating systems
+    #variables and value types (a named place for some data that will be used in a program - and can be used multiple times)
+    #functions, parameters and return values 
+    #iterations and conditionals (for & if statements)
+    #files, input and output
+    #image manipulation at the pixel level 
+    #analysis of texts through a library and an external API
+#Object has 2 things associated with it: variables and functions
+#function: way of storing and reusing bits of code
+#Blender and RenPy use Python
+#Why Python?
+    #interpreted (no compilation step)
+    #no scaffolding required
+    #support for classes and a robust module system (libraries)
+    #supports jupyter notebooks
+    #popular for machine learning 
+#Processing (2nd platform we will see/use today)
+    #framework for creative coding in multiple languages
+    #can be used for exploration and experimentation
+    #goals are to minimize friction and to maximize support for media programming
+    #"sketches" are project files that cotain code, assets, & a mode
+#Lab 2 common problems
+    
